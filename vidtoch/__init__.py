5f2a80779503d37a132e0f893d737d3d93c91553
hrpzcf <hrpzcf@foxmail.com>
2021-09-22 22:46:17 +0800

    release: 0.3.0

--- vidtoch/__init__.py
+++ vidtoch/__init__.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 
-from .__utils__ import makeVideo
+from .__utils__ import FFCmdUtils, makeVideo, vTools
 
 NAME = "vidtoch"
-VERSIONNUM = 0, 2, 3
+VERSIONNUM = 0, 3, 0
 VERSION = ".".join(map(str, VERSIONNUM))
 AUTHOR = "hrpzcf"
 EMAIL = "hrpzcf@foxmail.com"
 WEBSITE = "https://gitee.com/hrpzcf/vidtoch"
 
-__all__ = ["makeVideo"]
+__all__ = ["FFCmdUtils", "makeVideo", "vTools"]
